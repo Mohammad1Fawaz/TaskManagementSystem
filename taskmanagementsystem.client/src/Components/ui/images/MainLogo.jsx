245471464eaecd3fc577f7c7ed059f44aea90c6f
Mohammad Fawaz <Mohammad Fawaz@MOHAMMAD-PC>
2024-03-28 21:43:24 +0200

    small fixes

--- taskmanagementsystem.client/src/Components/ui/images/MainLogo.jsx
+++ taskmanagementsystem.client/src/Components/ui/images/MainLogo.jsx
@@ -2,7 +2,7 @@
 const MainLogo = ({ className }) => {
 
     return (
-        <img src="/src/assets/ToTaskLogo.png" alt="ToTask" className={`${className} object-contain`} />
+        <img src="/ToTaskLogo.png" alt="ToTask" className={`${className} object-contain`} />
     );
 }
 export default MainLogo;
